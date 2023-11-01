19e61bb8f195899f0dbdde156b228c9a708863a7
Seila Aillet <seila.aillet@epitech.eu>
2023-11-01 18:35:27 +0100

    %g

---
+++ internal_functions/display_double_for_number.c
@@ -0,0 +1,44 @@
+/*
+** EPITECH PROJECT, 2023
+** Created by seila.aillet@epitech.eu on 01/11/23.
+** File description:
+** display_double_for_number.c
+*/
+
+#include "../includes/base_lib.h"
+
+int find_zero(int i, double decimal_part)
+{
+    double tempo_decimal = decimal_part;
+
+    for (int n = i; n < 6; n++) {
+        tempo_decimal *= 10;
+        if ((int) tempo_decimal != 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void display_double_for_number(double a, int tempo)
+{
+    int rounded = (int) a;
+    double decimal_part = a - rounded;
+    int dotPrinted = 0;
+
+    if (decimal_part < 0)
+        decimal_part = decimal_part * (-1);
+    my_put_nbr(rounded);
+    for (int i = 0; i < 6; i++) {
+        decimal_part *= 10;
+        tempo = (int)decimal_part;
+        decimal_part = decimal_part - tempo;
+        if (tempo == 0 && find_zero(i, decimal_part) == 0)
+            break;
+        if (dotPrinted == 0) {
+            my_putstr(".");
+            dotPrinted = 1;
+        }
+        my_put_nbr(tempo);
+    }
+}
---
+++ internal_functions/display_double_number.c
@@ -0,0 +1,67 @@
+/*
+** EPITECH PROJECT, 2023
+** Created by seila.aillet@epitech.eu on 01/11/23.
+** File description:
+** display_double_number.c
+*/
+#include "../includes/base_lib.h"
+//#include "../includes/internal_functions.h"
+#include <stdio.h>
+
+void superior(double nb)
+{
+    int i = 0;
+    double tempo = nb;
+
+    for (; nb >= 10; i++) {
+        nb /= 10;
+    }
+    if (i >= 6) {
+        display_double_for_number(nb);
+        my_putchar('e');
+        my_putchar('+');
+        if (i <= 9) {
+            my_putchar('0');
+        }
+        my_put_nbr(i);
+    } else {
+        display_double_for_number(tempo);
+    }
+}
+
+void inferior(double nb)
+{
+    int i = 0;
+    double tempo = nb;
+
+    for (; nb < 1; i++) {
+        nb *= 10;
+    }
+    if (i >= 6) {
+        display_double_for_number(nb);
+        my_putchar('e');
+        my_putchar('-');
+        if (i <= 9) {
+            my_putchar('0');
+        }
+        my_put_nbr(i);
+    } else {
+        display_double_for_number(tempo);
+    }
+}
+
+void display_double_number(double a)
+{
+    double nb = a;
+
+    if (nb < 0) {
+        my_putchar('-');
+        nb = nb * (-1);
+    }
+    if (nb > 1) {
+        superior(nb);
+    }
+    if (nb < 1) {
+        inferior(nb);
+    }
+}
